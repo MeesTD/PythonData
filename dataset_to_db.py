b3d41bc5a45ad054d0e5d85dde44fb46e9a9fdbb
ReiraH <rheahau@gmail.com>
2023-05-26 14:04:07 +0200

    hotel, datum toegevoegd

--- dataset_to_db.py
+++ dataset_to_db.py
@@ -1,3 +1,4 @@
+from re import M
 import mysql.connector
 import csv
 
@@ -13,47 +14,40 @@ dbverbinding = mysql.connector.connect(
 # als connectie gemaakt is, het geeft niks terug, dan is het goed gelukt. een check.
 print("test")
 
-# tutorial van python 4 data verzamelen
-"""mijncursor = dbverbinding.cursor()
-mijncursor.execute("SELECT * FROM fiets")
-allefietsen = mijncursor.fetchall()
-print(allefietsen)"""
-
 # csv inlezen
 data1 = None
 with open('./csv_bestanden/hotel_bookings.csv', 'r') as f:
     data1 = csv.reader(f) # pakt alle rijen in een list
-    
 
     mijncursor = dbverbinding.cursor() # verbinding maken
     mijncursor.execute('DROP TABLE IF EXISTS hotel_booking;')
-    mijncursor.execute('CREATE TABLE hotel_booking(id INT not null AUTO_INCREMENT, PRIMARY KEY (id), arrival_date_year INT, arrival_date_month VARCHAR(30), arrival_date_day_of_month INT, meal VARCHAR(30), adr DECIMAL);')
+    mijncursor.execute('CREATE TABLE hotel_booking(id INT not null AUTO_INCREMENT, PRIMARY KEY (id), hotel VARCHAR(30), arrival_date_year INT, arrival_date_month VARCHAR(30), arrival_date_day_of_month INT, meal VARCHAR(30), adr DECIMAL, date DATE);')
     
-    # maakt nieuwe tabel aan met hotel_booking, id is primary key
-    #mijncursor.execute('CREATE TABLE hotel_booking(id INT not null AUTO_INCREMENT, PRIMARY KEY (id));')
-    # maakt tabel attribuut aan voor jaar, maand en land die overeenkomen met de dataset header
-    #mijncursor.execute('ALTER TABLE hotel_booking ADD (arrival_date_year INT, arrival_date_month VARCHAR(30), arrival_date_day_of_month INT, meal VARCHAR(30), adr DECIMAL)')
-
     # pakt voor elke rij de juiste waarden van jaar, maand en maaltijd en kosten uit de dataset
+    months = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+
+    i = 0
     for row in data1:
+        hotel = row[0]
         arrival_date_year = row[3]
         arrival_date_month = row[4]
         arrival_date_day_of_month = row[6]
         meal = row[12]
         adr = row[27]
+        month = months.get(arrival_date_month)
+        date = f"{arrival_date_year}-{month}-{arrival_date_day_of_month}"
+        
+        print(date)
         # voeg wwaarden toe aan de attributen in de tabel voor jaar, maand en land met de waarden die gelijk zijn aan het index nummer op die volgorde op de plek van %s
-        sql_insert_data = 'INSERT INTO hotel_booking (arrival_date_year, arrival_date_month, arrival_date_day_of_month, meal, adr) VALUES (%s, %s, %s, %s, %s);'
-        mijncursor.execute(sql_insert_data, (arrival_date_year, arrival_date_month, arrival_date_day_of_month, meal, adr)) # execute(SQL statement, (hier de waarden voor de plekken %s en op volgorde)))
+        
+        if i is not 0:
+            sql_insert_data = 'INSERT INTO hotel_booking (hotel, arrival_date_year, arrival_date_month, arrival_date_day_of_month, meal, adr, date) VALUES (%s, %s, %s, %s, %s, %s, %s);'
+            mijncursor.execute(sql_insert_data, (hotel, arrival_date_year, arrival_date_month, arrival_date_day_of_month, meal, adr, date)) # execute(SQL statement, (hier de waarden voor de plekken %s en op volgorde)))
     
-    # rijen toegevoegd bijhouden
-    aantal_rijen_ingevoegd = mijncursor.rowcount
-    print(aantal_rijen_ingevoegd)
+        i += 1
 
 # data opsturen naar de database
 dbverbinding.commit()
 
 # dataverbinding afsluiten
 dbverbinding.close()
-
-"""for f in allefietsen:
-    print(f[1])"""
