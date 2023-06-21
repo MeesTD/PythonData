ef92211aa71b2a7877d4b3a56932f499882b423a
MeesTD <55393026+MeesTD@users.noreply.github.com>
2023-06-21 14:50:36 +0200

    Add files via upload

---
+++ windowsgebruik.py
@@ -0,0 +1,11 @@
+def krijgpoort():
+    return 3306
+
+def krijgwachtwoord():
+    return 'DataYC2305'
+
+def krijgservernaam():
+    return "yc2305pythondb.mysql.database.azure.com"
+
+def krijguser():
+    return 'pythondb'
